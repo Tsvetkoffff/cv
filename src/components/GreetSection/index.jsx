ea8b0ed18a380f5805933936ff718c3761eb8a36
Slava Tsvetkoffff <v.tsvetkoffff@gmail.com>
2022-10-31 10:40:00 +0300

    fix: add greet-section__warpper to fix the position of the photo according to the design

--- src/components/GreetSection/index.jsx
+++ src/components/GreetSection/index.jsx
@@ -7,25 +7,27 @@ import {ReactComponent as ArrowDownIcon} from '/src/assets/icons/arrow-down.svg'
 const GreetingSection = () => {
   return (
     <section className='greet-section'>
-      <img
-        src={Photo}
-        alt='This is my photo'
-        className='greet-section__photo'
-      />
-      <div>
-        <p className='greet-section__status'>ready to work</p>
-        <h2 className='greet-section__heading'>
-          {'<Hello!/>'}
-          <br /> I am Slava
-          <br /> Frontend developer
-        </h2>
-        <div className='greet-section__buttons-wrapper'>
-          <Button text={'contact me'} mod='button--primary'>
-            <EnvelopIcon />
-          </Button>
-          <Button text={'download cv'} mod='button--white'>
-            <ArrowDownIcon />
-          </Button>
+      <div className='greet-section__warpper'>
+        <img
+          src={Photo}
+          alt='This is my photo'
+          className='greet-section__photo'
+        />
+        <div>
+          <p className='greet-section__status'>ready to work</p>
+          <h2 className='greet-section__heading'>
+            {'<Hello!/>'}
+            <br /> I am Slava
+            <br /> Frontend developer
+          </h2>
+          <div className='greet-section__buttons-wrapper'>
+            <Button text={'contact me'} mod='button--primary'>
+              <EnvelopIcon />
+            </Button>
+            <Button text={'download cv'} mod='button--white'>
+              <ArrowDownIcon />
+            </Button>
+          </div>
         </div>
       </div>
     </section>
