3c5ee55ed397c210ba7d11256170c09e27cf01a1
Slava Tsvetkoffff <v.tsvetkoffff@gmail.com>
2022-11-01 18:07:55 +0300

    feat: change about text content, fix some styles

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -8,14 +8,15 @@ const AboutMe = () => {
       <div className='about-section__wrapper py-9 space-y-5'>
         <SectionHeader text='About_me' dot={true} />
         <div className='description-wrapper flex flex-col space-y-7 md:space-y-0 md:flex-row-reverse  md:justify-end'>
-          <p className='about-section__description text-justify text-sm md:w-[40%]'>
-            I am a junior web developer, I live in Georgia. I am currently
-            looking for a full-time job as a remote junior web developer at an
-            IT company. I want to take on more work and improve my web developer
-            skills.
+          <p className='about-section__description text-justify text-sm box-border md:p-3 md:w-[46%]'>
+            I am a junior front-end developer, very passionate about development
+            and web technologies. Currently looking for commercial development
+            experience, preferably in a team of professionals. I am sure that
+            the experience gained in the learning process will be able to put
+            into practice and bring real benefits to your team and business.
           </p>
-          <div className='md:w-[40%]'>
-            <TagIcon/>
+          <div className='md:py-3 md:w-[40%]'>
+            <TagIcon />
           </div>
         </div>
       </div>
