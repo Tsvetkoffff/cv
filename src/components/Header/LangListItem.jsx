10096d17433dbc6679cbcf886e4d07f65f1d2585
Slava Tsvetkoffff <tsvetkoffff@gmail.com>
2022-11-22 13:19:26 +0300

    refactor: move the classes responsible for the appearance from the navigation to the appropriate components

--- src/components/Header/LangListItem.jsx
+++ src/components/Header/LangListItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LangListItem = ({text, setAreMenuClose}) => {
   return (
-    <li className='cursor-pointer hover:text-primary transition-colors p-2 text-base'>
+    <li className='cursor-pointer text-grey hover:text-primary transition-colors p-2 text-base font-medium'>
       <a href='#' onClick={() => setAreMenuClose(true)}>
         {text.toUpperCase()}
       </a>
--- src/components/Header/Navigation.jsx
+++ src/components/Header/Navigation.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navigation = ({areMenuClose, children}) => {
   return (
     <nav
-      className={`fixed flex flex-col space-y-10 z-10 h-full right-0 top-0 justify-center p-10 shadow-2xl bg-white box-border text-grey tracking-tightest font-medium transition-all duration-500 md:static md:flex-row md:space-y-0 md:p-0 md:shadow-none md:bg-none md:items-center md:flex-grow md:justify-end
+      className={`fixed flex flex-col space-y-10 z-10 h-full right-0 top-0 justify-center p-10 shadow-2xl bg-white box-border transition-all duration-500 md:static md:flex-row md:space-y-0 md:p-0 md:shadow-none md:bg-none md:items-center md:flex-grow md:justify-end
       ${areMenuClose && '-right-full'}`}
     >
       {children}
