d555f464798bf1718c597961f5df0d6c0030df21
Slava Tsvetkoffff <tsvetkoffff@gmail.com>
2022-11-22 13:17:06 +0300

    feat: improve the heading component

--- src/components/common/Heading.jsx
+++ src/components/common/Heading.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
-const Heading = ({Tag, dot, children}) => {
-  let size = ''
+const Heading = ({Tag, dot, children, className}) => {
+  const [size, setSize] = useState('')
 
-  if(Tag === 'h1' || Tag === 'h2') {
-    size = 'text-lg lg:text-xl xl:text-2xl'
-  } else if(Tag === 'h3') {
-    size = ''
-  }
+  useEffect(() => {
+    if (Tag === 'h1' || Tag === 'h2') {
+      setSize('text-2xl md:text-3xl lg:text-4xl xl:text-5xl')
+    } else if (Tag === 'h3') {
+      setSize('text-xl md:text-2xl lg:text-3xl')
+    }
+  }, [])
 
   return (
-    <Tag className='relative font-medium text-lg lg:text-xl xl:text-2xl'>
+    <Tag className={`relative font-medium ${className} ${size}`}>
       {children}
       {dot && <span className='text-primary'>.</span>}
     </Tag>
