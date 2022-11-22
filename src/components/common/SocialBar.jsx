3c36aa858da03239c41b1905cbf4223b14d0635d
Slava Tsvetkoffff <tsvetkoffff@gmail.com>
2022-11-22 16:10:33 +0300

    fix: shifted the social bar a little to the right

--- src/components/common/SocialBar.jsx
+++ src/components/common/SocialBar.jsx
@@ -28,7 +28,7 @@ const SocialBar = () => {
     },
   ]
   return (
-    <div className='hidden absolute z-10 md:block md:h-full md:top-0 md:right-[25px] lg:right-[45px] xl:right-[80px]'>
+    <div className='hidden absolute z-10 md:block md:h-full md:top-0 md:right-[25px] lg:right-[45px] xl:right-[70px]'>
       <div className='sticky md:flex md:flex-col md:top-[80px] lg:top-[106px] xl:top-[150px]'>
         {socials.map(item => (
           <a href={item.link} key={item.name}>
